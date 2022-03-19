43337bfaa7caeb743d4def6e750c1d14bb546f16
Montoya086 <mon21552@uvg.edu.gt>
2022-03-18 21:11:00 -0600

    Main y tienda

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 /**
  * 
  */
+import java.awt.LinearGradientPaint;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -30,15 +31,68 @@ public class Main {
 		}
 		Tienda tienda = new Tienda(nMap);
 		try {
-			FileReader r = new FileReader("datos.txt");
+			FileReader r = new FileReader("ListadoProducto.txt");
 			BufferedReader br = new BufferedReader(r);
 			String line;
 			while((line=br.readLine())!=null){
-				
+				String[] values = line.split("\\|");
+				tienda.inv_add(values[1], values[0]);
 			}
 			br.close();
 		}catch(IOException e) {
 			System.out.println("Ha ocurrido una excepcion de tipo IO: "+e);
+		}
+		
+		boolean salir = false;
+		while(!salir) {
+			int opt =0;
+			System.out.println("Ingrese la accion a realizar");
+			System.out.println("1. Agregar producto");
+			System.out.println("2. Mostrar categoria");
+			System.out.println("3. Mostrar carrito");
+			System.out.println("4. Mostrar carrito ordenado");
+			System.out.println("5. Mostrar inventario");
+			System.out.println("6. Mostrar inventario ordenado");
+			opt = Integer.parseInt(scan.nextLine());
+			switch (opt) {
+			case 1:
+				System.out.println("------------------------------------------------------------------------");
+				System.out.println("Ingrese el nombre del producto:");
+				String product = scan.nextLine();
+				tienda.cart_add(product);
+				System.out.println("------------------------------------------------------------------------");
+				break;
+			case 2:
+				System.out.println("------------------------------------------------------------------------");
+				System.out.println("Ingrese el nombre del producto:");
+				product = scan.nextLine();
+				System.out.println("El producto pertenece a: "+tienda.show_category(product));
+				System.out.println("------------------------------------------------------------------------");
+				break;
+			case 3:
+				System.out.println("------------------------------------------------------------------------");
+				System.out.println(tienda.show_cart_unsorted());
+				System.out.println("------------------------------------------------------------------------");
+				break;
+			case 4:
+				System.out.println("------------------------------------------------------------------------");
+				System.out.println(tienda.show_cart_sorted());
+				System.out.println("------------------------------------------------------------------------");
+				break;
+			case 5:
+				System.out.println("------------------------------------------------------------------------");
+				System.out.println(tienda.inv_show());
+				System.out.println("------------------------------------------------------------------------");
+				break;
+			case 6:
+				System.out.println("------------------------------------------------------------------------");
+				System.out.println(tienda.inv_show_sorted());
+				System.out.println("------------------------------------------------------------------------");
+				break;
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + opt);
+			}
+			
 		}
 
 	}
--- src/Tienda.java
+++ src/Tienda.java
@@ -1,5 +1,8 @@
 import java.util.Map;
-
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.ArrayList;
+import java.util.List;
 /**
  * 
  */
@@ -13,8 +16,76 @@ public class Tienda {
 	Map<String, Integer> cartMap;
 	FactoryMap<String, String> fInv= new FactoryMap<String, String>();
 	FactoryMap<String, Integer> fCart= new FactoryMap<String, Integer>();
+	int nmap;
+	/**
+	 * 
+	 * @param nMap
+	 */
 	public Tienda(int nMap) {
 		inventoryMap = fInv.instanceMap(nMap);
 		cartMap = fCart.instanceMap(nMap);
+		nmap = nMap;
+	}
+	public void inv_add(String product, String category) {
+		inventoryMap.put(product, category);
+	}
+	public void cart_add(String product) {
+		if (inventoryMap.containsKey(product)) {
+			boolean existscart=false;
+			for(String i: cartMap.keySet()) {
+				if(i.equals(product)) {
+					existscart=true;
+				}
+			}
+			if(existscart) {
+				int temp = cartMap.get(product);
+				cartMap.replace(product, temp+1);
+			}else {
+				cartMap.put(product, 1);
+			}
+			System.out.println("Producto agregado al carrito");
+		}else {
+			System.out.println("El producto no existe");
+		}
+		
+	}
+	public String inv_show() {
+		String invString ="";
+		for(String i: inventoryMap.keySet()) {
+			invString+=inventoryMap.get(i)+"    "+i+"\n";
+		}
+		return invString;
+	}
+	public String show_category(String product) {
+		return inventoryMap.get(product);
+	}
+	public String show_cart_unsorted() {
+		String cart_itemString="";
+		for(String i: inventoryMap.keySet()) {
+			if(cartMap.containsKey(i)) {
+				cart_itemString+="Categoria: "+inventoryMap.get(i)+"  Producto: "+i+"  Cantidad: "+ cartMap.get(i)+"\n";
+			}
+		}
+		return cart_itemString;
+	}
+	public String show_cart_sorted() {
+		String cart_itemString="";
+		Map<String, String> inventoryMap_sorted =fInv.instanceMap(nmap);
+		inventoryMap.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEachOrdered(x -> inventoryMap_sorted.put(x.getKey(), x.getValue()));
+		for(String i: inventoryMap_sorted.keySet()) {
+			if(cartMap.containsKey(i)) {
+				cart_itemString+="Categoria: "+inventoryMap.get(i)+"  Producto: "+i+"  Cantidad: "+ cartMap.get(i)+"\n";
+			}
+		}
+		return cart_itemString;
+	}
+	public String inv_show_sorted() {
+		String invString ="";
+		Map<String, String> inventoryMap_sorted =fInv.instanceMap(nmap);
+		inventoryMap.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEachOrdered(x -> inventoryMap_sorted.put(x.getKey(), x.getValue()));
+		for(String i: inventoryMap_sorted.keySet()) {
+			invString+=inventoryMap_sorted.get(i)+"    "+i+"\n";
+		}
+		return invString;
 	}
 }
